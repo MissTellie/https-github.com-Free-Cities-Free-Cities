5d05d2db557c88fd6d4c1f00a9b5b67cce844bec
prndev <prndev@users.noreply.github.com>
2017-08-03 12:11:57 +0200

    Changed compile_debug.bat into a wrapper around compile.bat to reduce redundancy.

--- compile_debug.bat
+++ compile_debug.bat
@@ -1,19 +1,3 @@
 @echo off
-:: Free Cities Basic Compiler - Windows
-:: Will wait for keypress before terminating.
-
-:: Will add all *.tw files to StoryIncludes.
-del src\config\start.tw
-copy src\config\start.tw.proto start.tw.tmp >nul
->>start.tw.tmp (for /r "src" %%F in (*.tw) do echo %%F)
-move start.tw.tmp src\config\start.tw >nul
-
-:: Run the appropriate compiler for the user's CPU architecture.
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
-    CALL "%~dp0devTools\tweeGo\tweego_win64.exe" -o "%~dp0bin/FC.html" "%~dp0src\config\start.tw"
-) else (
-    CALL "%~dp0devTools\tweeGo\tweego_win86.exe" -o "%~dp0bin/FC.html" "%~dp0src\config\start.tw"
-)
-
-ECHO Done
+call %~dp0compile.bat
 PAUSE
